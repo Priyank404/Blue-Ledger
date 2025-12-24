715fec3a0bf7eda8a68b529673d2c0b5c28cc856
Priyank404 <priyank.khambhati.06@gmail.com>
2025-12-24 20:42:01 +0530

    some config in authcontroller for setting cookie for production

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -16,8 +16,8 @@ export const signUp = async (req, res, next) =>{
 
         res.cookie('token', result.token, {
             httpOnly: true,
-            secure: false,
-            sameSite: 'lax',
+            secure: true,
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000
         })
 
