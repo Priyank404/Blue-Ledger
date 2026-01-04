7f690445d79b80dd9f1f4920bd7c020bde74acff
Priyank404 <priyank.khambhati.06@gmail.com>
2026-01-04 11:01:23 +0530

    url fix for cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ import globalErrorHandler from './middleWares/globalApiError.js';
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", "https://blue-legder.vercel.app/"],
+  origin: ["http://localhost:5173", "https://blue-legder.vercel.app"],
   credentials: true // because you're using cookies
 }));
 
