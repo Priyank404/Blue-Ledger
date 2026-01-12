29ab230a3325bdeb3c62452a353b80e5aff92db1
Priyank404 <priyank.khambhati.06@gmail.com>
2026-01-12 11:18:25 +0530

    portolio analytic route + testing done

--- Backend/src/Routes/portfolioRoutes.js
+++ Backend/src/Routes/portfolioRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleWares/verifyJWT.js";
 import { getPortfolioValueHistory } from "../controllers/portfolioController.js";
+import { getPorfolioAnalytics } from "../controllers/portfolioController.js";
 
 const router = Router();
 
 router.get("/value-history", verifyJWT, getPortfolioValueHistory);
+
+router.get("/analytic", verifyJWT, getPorfolioAnalytics)
 
 export default router;
